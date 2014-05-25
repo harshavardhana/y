c0416cc10dd3fb8ad4354241933144381bad52c4
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2014-05-25 00:28:54 -0700

    Separating moves from stones, adding history

--- client/y.js
+++ client/y.js
@@ -1,23 +1,34 @@
 Template.game.events({
   'click .undo_move': function() {
-    var moves = Moves.find().fetch();
-    var lastMove = _.max(moves, function(move) { return move.step; });
-    lastMove.step = 0;
-    Moves.update(lastMove._id, lastMove);
+    var lastMove = Moves.findOne({}, {sort: {step: -1}});
+    Moves.remove(lastMove._id);
   },
   'click .reset_board': function() {
     var moves = Moves.find().fetch();
     _.each(moves, function(move) {
-      move.step = 0;
-      Moves.update(move._id, move);
+      Moves.remove(move._id);
     });
-  },
+  }
 });
 
+/*
+Template.game.helpers({
+  listMoves: function() {
+    var moves = Moves.find({}, {sort: {step: 1}}).fetch();
+    console.log(moves);
+    var result = "";
+    for(var i = 0; i< moves.length; i++) {
+      result = result + moves[i].name;
+    }
+    return result;
+  }
+});
+*/
+
 Deps.autorun(function () {
-  var moves = Moves.find().fetch();
-  var lastMove = _.max(moves, function(move) { return move.step; });
-  if (lastMove.step > 0) {
+  //var lastMove = _.max(moves, function(move) { return move.step; });
+  var lastMove = Moves.findOne({}, {sort: {step: -1}});
+  if (typeof(lastMove) != 'undefined' && lastMove.step > 0) {
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'stone.ogg');
     audioElement.load();
@@ -29,18 +40,28 @@ Template.board.helpers({
   moves: function() {
     return Moves.find();
   },
+  stones: function() {
+    return Stones.find();
+  },
   lastMove: function() {
-    var moves = Moves.find().fetch();
-    var lastMove = _.max(moves, function(move) { return move.step; });
-    var lastmovestep = (lastMove.step > 1) ? [lastMove] : [];
-    return lastmovestep;
+    return [Moves.findOne({}, {sort: {step: -1}})];
+  }
+});
+
+Template.lastMoveTemplate.helpers({
+  cx: function() {
+    var stone = Stones.findOne({name: this.name});
+    return stone && stone.x;
+  },
+  cy: function() {
+    var stone = Stones.findOne({name: this.name});
+    return stone && stone.y;
   }
 });
 
 Template.move.helpers({
   color: function() {
-    var color;
-    color = (this.step === 0) ? "yellow" : (this.step % 2 === 0) ? "white" : "black";
+    var color = (this.step === 0) ? "yellow" : (this.step % 2 === 0) ? "white" : "black";
     return color;
   },
   opacity: function() {
@@ -50,17 +71,36 @@ Template.move.helpers({
   rsize: function() {
     var rsize = (this.step === 0) ? 17 : 10;
     return rsize;
+  },
+  cx: function() {
+    var stone = Stones.findOne({name: this.name});
+    return stone && stone.x;
+  },
+  cy: function() {
+    var stone = Stones.findOne({name: this.name});
+    return stone && stone.y;
   }
 });
 
-Template.move.events({
+Template.stone.events({
   'click': function() {
-    var moves = Moves.find().fetch();
-    var lastMove = _.max(moves, function(move) { return move.step; });
-    console.log(lastMove.step);
-    if(this.step === 0 || lastMove.step === 1) {
-      this.step = lastMove.step + 1;
-      Moves.update(this._id, this);
+    var lastMove = Moves.findOne({}, {sort: {step: -1}});
+    var step = 1;
+    if(typeof lastMove != 'undefined') {
+      step = lastMove.step + 1;
     }
+    if(step > 2) {
+      var conflictingMove = Moves.findOne({name: this.name});
+      if(typeof conflictingMove != 'undefined') {
+        return;
+      }
+    }
+    Moves.insert({name: this.name, step: step});
+  }
+});
+
+Template.listMoves.helpers({
+  moves: function() {
+    return Moves.find();
   }
 });
--- server/load_data.js
+++ server/load_data.js
@@ -1,110 +1,109 @@
-if(Moves.find().count() === 0) {
-    console.log("yes");
-    //row 1
-    Moves.insert({x:250, y: 17, step: 0});
-    //row 2
-    Moves.insert({x: 196, y: 44, step: 0});
-    Moves.insert({x: 250, y: 61, step: 0});
-    Moves.insert({x: 304, y: 44, step: 0});
-    //row 3
-    Moves.insert({x: 150, y: 77, step: 0});
-    Moves.insert({x: 201, y: 83, step: 0});
-    Moves.insert({x: 250, y: 99, step: 0});
-    Moves.insert({x: 299, y: 83, step: 0});
-    Moves.insert({x: 350, y: 77, step: 0});
-    //row 4
-    Moves.insert({x: 107, y: 119, step: 0});
-    Moves.insert({x: 158, y: 118, step: 0});
-    Moves.insert({x: 206, y: 123, step: 0});
-    Moves.insert({x: 250, y: 134, step: 0});
-    Moves.insert({x: 294, y: 123, step: 0});
-    Moves.insert({x: 342, y: 118, step: 0});
-    Moves.insert({x: 393, y: 119, step: 0});
-    //row 5
-    Moves.insert({x: 70, y: 171, step: 0});
-    Moves.insert({x: 119, y: 163, step: 0});
-    Moves.insert({x: 167, y: 161, step: 0});
-    Moves.insert({x: 209, y: 163, step: 0});
-    Moves.insert({x: 250, y: 172, step: 0});
-    Moves.insert({x: 291, y: 163, step: 0});
-    Moves.insert({x: 333, y: 161, step: 0});
-    Moves.insert({x: 381, y: 163, step: 0});
-    Moves.insert({x: 430, y: 171, step: 0});
-    //row 6
-    Moves.insert({x: 43, y: 230, step: 0});
-    Moves.insert({x: 87, y: 219, step: 0});
-    Moves.insert({x: 133, y: 208, step: 0});
-    Moves.insert({x: 178, y: 203, step: 0});
-    Moves.insert({x: 224, y: 209, step: 0});
-    Moves.insert({x: 276, y: 209, step: 0});
-    Moves.insert({x: 322, y: 203, step: 0});
-    Moves.insert({x: 367, y: 208, step: 0});
-    Moves.insert({x: 413, y: 219, step: 0});
-    Moves.insert({x: 457, y: 230, step: 0});
-    //row 7
-    Moves.insert({x: 28, y: 288, step: 0});
-    Moves.insert({x: 68, y: 274, step: 0});
-    Moves.insert({x: 109, y: 260, step: 0});
-    Moves.insert({x: 151, y: 249, step: 0});
-    Moves.insert({x: 201, y: 247, step: 0});
-    Moves.insert({x: 250, y: 250, step: 0});
-    Moves.insert({x: 299, y: 247, step: 0});
-    Moves.insert({x: 349, y: 249, step: 0});
-    Moves.insert({x: 109, y: 260, step: 0});
-    Moves.insert({x: 391, y: 260, step: 0});
-    Moves.insert({x: 432, y: 274, step: 0});
-    Moves.insert({x: 472, y: 288, step: 0});
-    //row 8
-    Moves.insert({x: 22, y: 344, step: 0});
-    Moves.insert({x: 59, y: 329, step: 0});
-    Moves.insert({x: 96, y: 313, step: 0});
-    Moves.insert({x: 132, y: 297, step: 0});
-    Moves.insert({x: 180, y: 287, step: 0});
-    Moves.insert({x: 227, y: 289, step: 0});
-    Moves.insert({x: 273, y: 289, step: 0});
-    Moves.insert({x: 320, y: 287, step: 0});
-    Moves.insert({x: 368, y: 297, step: 0});
-    Moves.insert({x: 404, y: 313, step: 0});
-    Moves.insert({x: 441, y: 329, step: 0});
-    Moves.insert({x: 478, y: 344, step: 0});
-    //row 9
-    Moves.insert({x: 26, y: 405, step: 0});
-    Moves.insert({x: 64, y: 383, step: 0});
-    Moves.insert({x: 97, y: 364, step: 0});
-    Moves.insert({x: 128, y: 346, step: 0});
-    Moves.insert({x: 160, y: 327, step: 0});
-    Moves.insert({x: 205, y: 331, step: 0});
-    Moves.insert({x: 250, y: 332, step: 0});
-    Moves.insert({x: 295, y: 331, step: 0});
-    Moves.insert({x: 340, y: 327, step: 0});
-    Moves.insert({x: 372, y: 346, step: 0});
-    Moves.insert({x: 403, y: 364, step: 0});
-    Moves.insert({x: 436, y: 383, step: 0});
-    Moves.insert({x: 474, y: 405, step: 0});
-    //row 10
-    Moves.insert({x: 76, y: 438, step: 0});
-    Moves.insert({x: 108, y: 414, step: 0});
-    Moves.insert({x: 140, y: 390, step: 0});
-    Moves.insert({x: 172, y: 367, step: 0});
-    Moves.insert({x: 223, y: 375, step: 0});
-    Moves.insert({x: 277, y: 375, step: 0});
-    Moves.insert({x: 328, y: 367, step: 0});
-    Moves.insert({x: 360, y: 390, step: 0});
-    Moves.insert({x: 392, y: 414, step: 0});
-    Moves.insert({x: 424, y: 438, step: 0});
-    //row 11
-    Moves.insert({x: 128, y: 462, step: 0});
-    Moves.insert({x: 160, y: 434, step: 0});
-    Moves.insert({x: 193, y: 404, step: 0});
-    Moves.insert({x: 250, y: 410, step: 0});
-    Moves.insert({x: 307, y: 404, step: 0});
-    Moves.insert({x: 340, y: 434, step: 0});
-    Moves.insert({x: 372, y: 462, step: 0});
-    //row 12
-    Moves.insert({x: 186, y: 477, step: 0});
-    Moves.insert({x: 218, y: 445, step: 0});
-    Moves.insert({x: 282, y: 445, step: 0});
-    Moves.insert({x: 314, y: 477, step: 0});
-    //row 13
-    Moves.insert({x: 250, y: 483, step: 0});
+if(Stones.find().count() === 0) {
+    //row A
+    Stones.insert({name: "A1", x:250, y: 17});
+    //row B
+    Stones.insert({name: "B1", x: 196, y: 44});
+    Stones.insert({name: "B2", x: 250, y: 61});
+    Stones.insert({name: "B3", x: 304, y: 44});
+    //row C
+    Stones.insert({name: "C1", x: 150, y: 77});
+    Stones.insert({name: "C2", x: 201, y: 83});
+    Stones.insert({name: "C3", x: 250, y: 99});
+    Stones.insert({name: "C4", x: 299, y: 83});
+    Stones.insert({name: "C5", x: 350, y: 77});
+    //row D
+    Stones.insert({name: "D1", x: 107, y: 119});
+    Stones.insert({name: "D2", x: 158, y: 118});
+    Stones.insert({name: "D3", x: 206, y: 123});
+    Stones.insert({name: "D4", x: 250, y: 134});
+    Stones.insert({name: "D5", x: 294, y: 123});
+    Stones.insert({name: "D6", x: 342, y: 118});
+    Stones.insert({name: "D7", x: 393, y: 119});
+    //row E
+    Stones.insert({name: "E1", x: 70, y: 171});
+    Stones.insert({name: "E2", x: 119, y: 163});
+    Stones.insert({name: "E3", x: 167, y: 161});
+    Stones.insert({name: "E4", x: 209, y: 163});
+    Stones.insert({name: "E5", x: 250, y: 172});
+    Stones.insert({name: "E6", x: 291, y: 163});
+    Stones.insert({name: "E7", x: 333, y: 161});
+    Stones.insert({name: "E8", x: 381, y: 163});
+    Stones.insert({name: "E9", x: 430, y: 171});
+    //row F
+    Stones.insert({name: "F1", x: 43, y: 230});
+    Stones.insert({name: "F2", x: 87, y: 219});
+    Stones.insert({name: "F3", x: 133, y: 208});
+    Stones.insert({name: "F4", x: 178, y: 203});
+    Stones.insert({name: "F5", x: 224, y: 209});
+    Stones.insert({name: "F6", x: 276, y: 209});
+    Stones.insert({name: "F7", x: 322, y: 203});
+    Stones.insert({name: "F8", x: 367, y: 208});
+    Stones.insert({name: "F9", x: 413, y: 219});
+    Stones.insert({name: "F10", x: 457, y: 230});
+    //row G
+    Stones.insert({name: "G1", x: 28, y: 288});
+    Stones.insert({name: "G2", x: 68, y: 274});
+    Stones.insert({name: "G3", x: 109, y: 260});
+    Stones.insert({name: "G4", x: 151, y: 249});
+    Stones.insert({name: "G5", x: 201, y: 247});
+    Stones.insert({name: "G6", x: 250, y: 250});
+    Stones.insert({name: "G7", x: 299, y: 247});
+    Stones.insert({name: "G8", x: 349, y: 249});
+    Stones.insert({name: "G9", x: 109, y: 260});
+    Stones.insert({name: "G10", x: 391, y: 260});
+    Stones.insert({name: "G11", x: 432, y: 274});
+    Stones.insert({name: "G12", x: 472, y: 288});
+    //row H
+    Stones.insert({name: "H1", x: 22, y: 344});
+    Stones.insert({name: "H2", x: 59, y: 329});
+    Stones.insert({name: "H3", x: 96, y: 313});
+    Stones.insert({name: "H4", x: 132, y: 297});
+    Stones.insert({name: "H5", x: 180, y: 287});
+    Stones.insert({name: "H6", x: 227, y: 289});
+    Stones.insert({name: "H7", x: 273, y: 289});
+    Stones.insert({name: "H8", x: 320, y: 287});
+    Stones.insert({name: "H9", x: 368, y: 297});
+    Stones.insert({name: "H10", x: 404, y: 313});
+    Stones.insert({name: "H11", x: 441, y: 329});
+    Stones.insert({name: "H12", x: 478, y: 344});
+    //row I
+    Stones.insert({name: "I1", x: 26, y: 405});
+    Stones.insert({name: "I2", x: 64, y: 383});
+    Stones.insert({name: "I3", x: 97, y: 364});
+    Stones.insert({name: "I4", x: 128, y: 346});
+    Stones.insert({name: "I5", x: 160, y: 327});
+    Stones.insert({name: "I6", x: 205, y: 331});
+    Stones.insert({name: "I7", x: 250, y: 332});
+    Stones.insert({name: "I8", x: 295, y: 331});
+    Stones.insert({name: "I9", x: 340, y: 327});
+    Stones.insert({name: "I10", x: 372, y: 346});
+    Stones.insert({name: "I11", x: 403, y: 364});
+    Stones.insert({name: "I12", x: 436, y: 383});
+    Stones.insert({name: "I13", x: 474, y: 405});
+    //row J
+    Stones.insert({name: "J1", x: 76, y: 438});
+    Stones.insert({name: "J2", x: 108, y: 414});
+    Stones.insert({name: "J3", x: 140, y: 390});
+    Stones.insert({name: "J4", x: 172, y: 367});
+    Stones.insert({name: "J5", x: 223, y: 375});
+    Stones.insert({name: "J6", x: 277, y: 375});
+    Stones.insert({name: "J7", x: 328, y: 367});
+    Stones.insert({name: "J8", x: 360, y: 390});
+    Stones.insert({name: "J9", x: 392, y: 414});
+    Stones.insert({name: "J10", x: 424, y: 438});
+    //row K
+    Stones.insert({name: "K1", x: 128, y: 462});
+    Stones.insert({name: "K2", x: 160, y: 434});
+    Stones.insert({name: "K3", x: 193, y: 404});
+    Stones.insert({name: "K4", x: 250, y: 410});
+    Stones.insert({name: "K5", x: 307, y: 404});
+    Stones.insert({name: "K6", x: 340, y: 434});
+    Stones.insert({name: "K7", x: 372, y: 462});
+    //row L
+    Stones.insert({name: "L1", x: 186, y: 477});
+    Stones.insert({name: "L2", x: 218, y: 445});
+    Stones.insert({name: "L3", x: 282, y: 445});
+    Stones.insert({name: "L4", x: 314, y: 477});
+    //row M
+    Stones.insert({name: "M1", x: 250, y: 483});
   }

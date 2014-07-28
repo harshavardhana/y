a207d84c180e535301278b60f37103fafad94c0b
Kenton Varda <temporal@gmail.com>
2014-07-28 15:16:59 -0700

    Play nice with non-username accounts.
    This change doesn't actually enable non-username accounts, but just makes sure the code does reasonable things if they are later enabled. This includes using the user's profile name for display purposes and not rejecting non-username account creation.

--- server/users.js
+++ server/users.js
@@ -1,9 +1,11 @@
 Accounts.validateNewUser(function (user) {
-  if (user.username === "root" || user.username === "admin")
-    throw new Meteor.Error(403, "Username used is a reserved word");
-  if (user.username.length < 3)
-    throw new Meteor.Error(403, "Username must have at least 3 characters");
-  if (user.username.length > 32)
-    throw new Meteor.Error(403, "Username can be upto 32 characters");
+  if (user.username) {
+    if (user.username === "root" || user.username === "admin")
+      throw new Meteor.Error(403, "Username used is a reserved word");
+    if (user.username.length < 3)
+      throw new Meteor.Error(403, "Username must have at least 3 characters");
+    if (user.username.length > 32)
+      throw new Meteor.Error(403, "Username can be upto 32 characters");
+  }
   return true;
 });
